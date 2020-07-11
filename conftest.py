3a3c34ddceb4f8292e3cc0aea8b5874869b63d30
molnunge <molnunge@gmail.com>
2020-07-11 10:24:32 +0300

    disable captcha via ftp config change

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 import json
 import os.path
 from fixture.applicaton import Application
+import ftputil
 import importlib
 
 fixture = None
@@ -15,16 +16,42 @@ def load_config(file):
             target = json.load(f)
     return target
 
+@pytest.fixture(scope="session")
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
 @pytest.fixture
-def app(request):
+def app(request, config):
 #    fixture = Application()
 #    fixture.session.login(username="admin", passw="secret")
     global fixture
     browser = request.config.getoption("--browser")
-    web_config = load_config(request.config.getoption("--target"))['web']
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=web_config['baseUrl'])
+        fixture = Application(browser=browser, base_url=config['web']['baseUrl'])
     return fixture
+
+@pytest.fixture(scope="session", autouse=True)
+def configure_server(request, config):
+    install_server_configuration(config["ftp"]["host"], config["ftp"]["username"], config["ftp"]["password"])
+    def fin():
+        restore_server_configuration(config["ftp"]["host"], config["ftp"]["username"], config["ftp"]["password"])
+    request.addfinalizer(fin)
+
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_defaults_inc.php.bak"):
+            remote.remove("config_defaults_inc.php.bak")
+        if remote.path.isfile("config_defaults_inc.php"):
+            remote.rename("config_defaults_inc.php", "config_defaults_inc.php.bak")
+        remote.upload(os.path.join(os.path.dirname(__file__), "resources/config_defaults_inc.php"), "config_defaults_inc.php")
+
+def restore_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_defaults_inc.php.bak"):
+            if remote.path.isfile("config_defaults_inc.php"):
+                remote.remove("config_defaults_inc.php")
+            remote.rename("config_defaults_inc.php.bak", "config_defaults_inc.php")
+
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
