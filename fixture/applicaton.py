86c9d74dc9a6657beb72e00719b1bf1afca64ed2
molnunge <molnunge@gmail.com>
2020-07-21 23:05:39 +0300

    added example of saop implementation
    for verification of user login

--- fixture/applicaton.py
+++ fixture/applicaton.py
@@ -3,6 +3,7 @@ from fixture.session import SessionHelper
 from fixture.james import JamesHelper
 from fixture.mail import MailHelper
 from fixture.signup import SignupHelper
+from fixture.soap import SoapHelper
 
 print("AppStart")
 class Application:
@@ -21,6 +22,7 @@ class Application:
         self.james = JamesHelper(self)
         self.signup = SignupHelper(self)
         self.mail = MailHelper(self)
+        self.soap = SoapHelper(self)
         self.config = config
         self.base_url=config['web']['baseUrl']
 
--- tests/signup_new.py
+++ tests/signup_new.py
@@ -11,7 +11,11 @@ def test_signup_new_account(app):
     password = "test"
     app.james.ensure_user_exists(username, password)
     app.signup.signup_new_user(username, email, password)
-    app.session.login(username, password)
-    assert app.session.is_logged_in_as(username)
-    app.session.logout()
+
+    assert app.soap.can_login(username, password)
+
+
+    # app.session.login(username, password)
+    # assert app.session.is_logged_in_as(username)
+    # app.session.logout()
 
